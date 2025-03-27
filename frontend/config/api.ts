0ab9ff253232cbcf85ba4f3b9d1db3f3a9ffbea2
rprakashdass <rprakashdass@gmai.com>
2025-03-27 19:06:06 +0530

    fix update

--- frontend/config/api.ts
+++ frontend/config/api.ts
@@ -1,4 +1,4 @@
-// const SERVER_URL = "https://group-links.onrender.com";
-const SERVER_URL = "http://localhost:3000";
+const SERVER_URL = "https://group-links.onrender.com";
+// const SERVER_URL = "http://localhost:3000";
 
 export default SERVER_URL;
